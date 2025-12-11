6902a54a66413363d2d670c061bcb7edbae244e5
hugemobster <patelaryan05@gmail.com>
2025-12-11 11:58:56 -0500

    Stil trying to pass lint tests

--- service/routes.py
+++ service/routes.py
@@ -10,8 +10,6 @@ from service.common import status  # HTTP Status Codes
 from . import app  # Import Flask application
 
 
-
-
 ############################################################
 # Health Endpoint
 ############################################################
@@ -63,6 +61,7 @@ def create_accounts():
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
+
 # ... place you code here to LIST accounts ...
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
@@ -80,6 +79,7 @@ def list_accounts():
 ######################################################################
 # READ AN ACCOUNT
 ######################################################################
+
 # ... place you code here to READ an account ...
 @app.route("/accounts/<int:account_id>", methods=["GET"])
 def get_accounts(account_id):
@@ -97,6 +97,7 @@ def get_accounts(account_id):
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
+
 # ... place you code here to UPDATE an account ...
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
@@ -109,12 +110,14 @@ def update_accounts(account_id):
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
     account.deserialize(request.get_json())
-
-        account.update()
+    account.update()
     return account.serialize(), status.HTTP_200_OK
+
+
 ######################################################################
 # DELETE AN ACCOUNT
 ######################################################################
+
 # ... place you code here to DELETE an account ...
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
 def delete_accounts(account_id):
@@ -125,13 +128,15 @@ def delete_accounts(account_id):
     app.logger.info("Request to delete an Account with id: %s", account_id)
     account = Account.find(account_id)
     if account:
-
         account.delete()
     return "", status.HTTP_204_NO_CONTENT
+
 
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
 ######################################################################
+
+
 def check_content_type(media_type):
     """Checks that the media type is correct"""
     content_type = request.headers.get("Content-Type")
@@ -139,8 +144,6 @@ def check_content_type(media_type):
         return
     app.logger.error("Invalid Content-Type: %s", content_type)
     abort(
-     
-       status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
-
+        status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
         f"Content-Type must be {media_type}",
     )
