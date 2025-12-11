b3592b0f152e9da1ebf5cec921afdd19c610b83c
hugemobster <patelaryan05@gmail.com>
2025-12-11 11:52:27 -0500

    added lines to match flake8 E302 and W292

--- service/routes.py
+++ service/routes.py
@@ -8,6 +8,10 @@ from flask import jsonify, request, make_response, abort, url_for   # noqa; F401
 from service.models import Account
 from service.common import status  # HTTP Status Codes
 from . import app  # Import Flask application
+
+
+
+
 ############################################################
 # Health Endpoint
 ############################################################
@@ -15,6 +19,8 @@ from . import app  # Import Flask application
 def health():
     """Health Status"""
     return jsonify(dict(status="OK")), status.HTTP_200_OK
+
+
 ######################################################################
 # GET INDEX
 ######################################################################
@@ -29,6 +35,8 @@ def index():
         ),
         status.HTTP_200_OK,
     )
+
+
 ######################################################################
 # CREATE A NEW ACCOUNT
 ######################################################################
@@ -50,6 +58,8 @@ def create_accounts():
     return make_response(
         jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
     )
+
+
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
@@ -65,6 +75,8 @@ def list_accounts():
     account_list = [account.serialize() for account in accounts]
     app.logger.info("Returning [%s] accounts", len(account_list))
     return jsonify(account_list), status.HTTP_200_OK
+
+
 ######################################################################
 # READ AN ACCOUNT
 ######################################################################
@@ -80,6 +92,8 @@ def get_accounts(account_id):
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
     return account.serialize(), status.HTTP_200_OK
+
+
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
@@ -95,7 +109,8 @@ def update_accounts(account_id):
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
     account.deserialize(request.get_json())
-    account.update()
+
+        account.update()
     return account.serialize(), status.HTTP_200_OK
 ######################################################################
 # DELETE AN ACCOUNT
@@ -110,6 +125,7 @@ def delete_accounts(account_id):
     app.logger.info("Request to delete an Account with id: %s", account_id)
     account = Account.find(account_id)
     if account:
+
         account.delete()
     return "", status.HTTP_204_NO_CONTENT
 
@@ -123,6 +139,8 @@ def check_content_type(media_type):
         return
     app.logger.error("Invalid Content-Type: %s", content_type)
     abort(
-        status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
+     
+       status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
+
         f"Content-Type must be {media_type}",
     )
